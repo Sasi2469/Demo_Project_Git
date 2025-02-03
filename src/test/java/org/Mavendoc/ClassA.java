6d5944a7a7de9fc3d306e0622dac0ea3e5df13ea
Sasi2469 <sasik288120@gmail.com>
2025-02-03 19:00:21 +0530

    new project demo

---
+++ src/test/java/org/Mavendoc/ClassA.java
@@ -0,0 +1,14 @@
+package org.Mavendoc;
+
+public class ClassA {
+
+	
+	public static void method1() {
+		System.out.println("demo");
+
+	}
+	
+	public static void main(String[] args) {
+		method1();
+	}
+}
