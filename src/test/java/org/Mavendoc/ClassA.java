894b09f93dbd008fee66d0afd7ff79819a7f06a7
Sasi2469 <sasik288120@gmail.com>
2025-02-04 18:49:51 +0530

    changes made

--- src/test/java/org/Mavendoc/ClassA.java
+++ src/test/java/org/Mavendoc/ClassA.java
@@ -18,10 +18,15 @@ public class ClassA {
 		System.out.println("demo145");
 
 	}
+	public static void method6() {
+		System.out.println("demo869");
+
+	}
 	
 	public static void main(String[] args) {
 		method1();
 		method2();
 		method3();
+		method6();
 	}
 }
